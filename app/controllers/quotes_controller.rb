ac317cbe01426074bf0b16007418f14b08fd9a4d
JennyOh1004 <ohseulki1004@gmail.com>
2019-08-19 23:56:35 +0000

    Initial commit

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,23 @@
 class QuotesController < ApplicationController
   def index
   	@quote = Quote.order("RANDOM()").first
-  end	
+  end
+
+  def new
+  	@quote = Quote.new
+  end
+
+  def create
+  	Quote.create(quote_params)
+  	#sends the saying and author parts to the database.
+  	redirect_to root_path
+  end
+
+  private
+
+  def quote_params
+  	#pulls the values of saying and author from the quotes form.
+  	params.require(:quote).permit(:saying, :author)
+    #make sure that our form is secure(we only permit "saying" and "author")
+  end
 end
