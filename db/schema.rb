91c11057fda12df092d72eab7f5e2a7bdeab1434
“JennyOh1004” <ohseulki1004@gmail.com>
2019-12-02 01:43:43 +0000

    Splurty quote

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_23_003115) do
+ActiveRecord::Schema.define(version: 2019_10_25_201913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "grams", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "quotes", force: :cascade do |t|
     t.string "saying"
